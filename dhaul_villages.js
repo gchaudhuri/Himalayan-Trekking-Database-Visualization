fb6ee83f8fae3972f56cb2c68377d766053e712e
Gargi Chaudhuri <39960842+gchaudhuri@users.noreply.github.com>
2022-01-13 14:52:22 -0600

    Add files via upload

---
+++ dhaul_villages.js
@@ -0,0 +1,11 @@
+var dhaul_villages = {
+"type": "FeatureCollection",
+"name": "dhaulagiri_villages",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Muri", "description": null, "altitudeMode": "clampToGround", "longitude": "29", "FID": "0", "latitude": "83", "Dhaul_vill": "Muri", "Id": "0", "Field_1": "Muri" }, "geometry": { "type": "Point", "coordinates": [ 83.365947310574398, 28.519868471935052 ] } },
+{ "type": "Feature", "properties": { "Name": "Mukut", "description": null, "altitudeMode": "clampToGround", "longitude": "29", "FID": "1", "latitude": "83", "Dhaul_vill": "Mukut", "Id": "0", "Field_1": "Mukut" }, "geometry": { "type": "Point", "coordinates": [ 83.267142464502697, 28.882628447567399 ] } },
+{ "type": "Feature", "properties": { "Name": "Tarakot", "description": null, "altitudeMode": "clampToGround", "longitude": "29", "FID": "2", "latitude": "83", "Dhaul_vill": "Tarakot", "Id": "0", "Field_1": "Tarakot" }, "geometry": { "type": "Point", "coordinates": [ 82.98285622367753, 28.880792279423471 ] } },
+{ "type": "Feature", "properties": { "Name": "Gurja", "description": null, "altitudeMode": "clampToGround", "longitude": "29", "FID": "3", "latitude": "83", "Dhaul_vill": "Gurja", "Id": "0", "Field_1": "Gurja" }, "geometry": { "type": "Point", "coordinates": [ 83.235302094580177, 28.589499501149799 ] } }
+]
+};
